d3ab169efcc075e49d735e83cd1b7be1be5911c7
Pedro Henrique Cerqueira Fernandes <pedrohcf3.14@gmail.com>
2020-04-03 08:29:51 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,17 @@
 class Pesoa:
+    def __init__(self,nome = None, idade = 33):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Ola Mundo{id(self)}'
 
 if __name__ == '__main__':
-    p = Pesoa()
+    p = Pesoa('Henrique')
     print(Pesoa.cumprimentar(p))
     print(id(p) )
     print(p.cumprimentar())
-
-
+    print(p.nome)
+    p.nome = 'Pedro'
+    print(p.nome)
+    print(p.idade)
